d79cd6b9e0b9963b502ddaf44dbf2d258536b0de
Yitzak DEKPEMOU <yitzak01@gmail.com>
2016-04-20 08:32:01 +0000

    Added files via upload

---
+++ templates/emails/changemail.tmpl.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<h1>Nouvelle adresse Email!</h1>
+<p>Vous avez ajouter cette adresse email comme adresse principale de votre compte <?= WEBSITE_NAME ?>!</p>
+</body>
+</html>
---
+++ templates/emails/newmail.tmpl.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<h1>Enregistrement de votre adresse!</h1>
+<p>Vous avez ajouter une nouvelle adresse email à votre compte <?= WEBSITE_NAME ?>!</p>
+</body>
+</html>
