7c55638659ac3ad05b57a4bf83b8a7f261f7fdac
Yitzak DEKPEMOU <yitzak01@gmail.com>
2016-04-20 08:35:59 +0000

    Added files via upload

---
+++ locales/menu.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * User: Yitzak DEKPEMOU
+*/
+
+$menu = [
+
+    'accueil' => ['fr' => 'Accueil', 'en' => 'Home'],
+    'connexion' => ['fr' => 'Connecion', 'en' => 'Login'],
+    'inscription' => ['fr' => 'Inscription', 'en' => 'Register'],
+    'profile' => ['fr' => 'Profile', 'en' => 'Account'],
+    'contact' => ['fr' => 'Contacts', 'en' => 'Contacts'],
+    'dashboard' => ['fr' => 'Tableau de bord', 'en' => 'Dashboard'],
+    'new.boards' => ['fr' => 'Nouveau tableau', 'en' => 'New board'],
+    'my.boards' => ['fr' => 'Mes tableaux', 'en' => 'My boards'],
+    'board' => ['fr' => 'Tableau', 'en' => 'board'],
+    'settings' => ['fr' => 'Mes paramètres', 'en' => 'My settings'],
+    'users' => ['fr' => 'Tous les utilisateurs', 'en' => 'All users'],
+    'nouveau' => ['fr' => 'Nouveau tableau', 'en' => 'New board'],
+    'deconnexion' => ['fr' => '<i class="fa fa-sign-out"></i>&nbsp;Déconnexion', 'en' => '<i class="fa fa-sign-out"></i>&nbsp;Logout'],
+
+];
